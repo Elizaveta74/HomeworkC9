b76c3ea97ec09c8e48d61ef1f465c28df37077b4
Elizaveta <lizasid@mail.ru>
2022-09-02 23:26:27 +0300

    Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -3,15 +3,22 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
 
+long CalcSum(int m, int n)
+{
+    if (m > n)
+    {
+       return 0; 
+    } 
+    else
+    {
+        return m + CalcSum(m + 1, n);
+    } 
+}
+
+
 Console.WriteLine("Введите M");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите N");
 int n = Convert.ToInt32(Console.ReadLine());
-
-void PrintNumbersToN(int n)
-{
-    if (n < 1) return;
-
-    Console.Write($"{n} ");
-    PrintNumbersToN(n - 1);
-}
+long sum = CalcSum(m, n);
+Console.WriteLine($"Сумма значений натуральных чисел от {m} до {n} = {sum}");
